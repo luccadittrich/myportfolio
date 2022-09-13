9f6429ef86a679d5ce49c1009e2581b9f1b67df5
lucca dittrich <dit21003@byui.edu>
2022-09-13 18:47:06 -0300

    added curriculum

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  get 'home/download_pdf'
 
   resources :pages, only: %i[index show]
 end
